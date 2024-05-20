ba8a73700f4496efe53c97010dc6292338ae9ea7
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-05-20 21:40:19 +0300

    'upd'

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
+
+import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import './css/loader.css';
+
+import iconError from './img/icon-error.svg';
+import iconClose from './img/icon-close-white.svg';
